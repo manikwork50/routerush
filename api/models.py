019c0b2295b7534107d89bf2ca31d04023754de4
manikwork50 <manikwork50@gmail.com>
2025-07-16 19:23:33 +0530

    Update models.py

--- api/models.py
+++ api/models.py
@@ -4,9 +4,9 @@ db = SQLAlchemy()
 
 class HiddenGem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    state = db.Column(db.String(100))
-    name = db.Column(db.String(100))
-    description = db.Column(db.Text)
+    state = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.String(255))
     submitted_by = db.Column(db.String(100))
 
@@ -20,3 +20,17 @@ class HiddenGem(db.Model):
             'submitted_by': self.submitted_by
         }
 
+class EmergencyContact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    state = db.Column(db.String(100), nullable=False)
+    police = db.Column(db.String(20))
+    hospital = db.Column(db.String(20))
+    women_helpline = db.Column(db.String(20))
+
+    def serialize(self):
+        return {
+            'state': self.state,
+            'police': self.police,
+            'hospital': self.hospital,
+            'women_helpline': self.women_helpline
+        }
