8ffbd78eaeae06baad5813435caf5793707a8d08
manikwork50 <manikwork50@gmail.com>
2025-07-16 19:21:35 +0530

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,1 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class HiddenGem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    state = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    image_url = db.Column(db.String(255))
+    submitted_by = db.Column(db.String(100))
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'state': self.state,
+            'name': self.name,
+            'description': self.description,
+            'image_url': self.image_url,
+            'submitted_by': self.submitted_by
+        }
 
