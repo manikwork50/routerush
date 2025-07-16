9532bc9694731aa319084aa45492b096596b26e7
manikwork50 <manikwork50@gmail.com>
2025-07-16 19:28:32 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from api.models import db
 from api.routes.hidden_gems import hidden_gems_bp
+from api.routes.emergency import emergency_bp  # <-- add this
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
@@ -9,14 +10,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-# Register route blueprint
+# Register route blueprints
 app.register_blueprint(hidden_gems_bp)
+app.register_blueprint(emergency_bp)
 
 @app.route('/')
 def home():
     return "🚀 WanderMap API is running!"
 
-# Create tables on first run
 @app.before_first_request
 def create_tables():
     db.create_all()
