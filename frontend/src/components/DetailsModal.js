9fc6240949db8841092d45a343c10bfe189b318d
manikwork50 <manikwork50@gmail.com>
2025-08-16 20:29:13 +0530

    Update DetailsModal.js

--- frontend/src/components/DetailsModal.js
+++ frontend/src/components/DetailsModal.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import Modal from "react-modal";
+
+export default function DetailsModal({ isOpen, details, onClose }) {
+  return (
+    <Modal isOpen={isOpen} onRequestClose={onClose} contentLabel="Route Details">
+      <h3>🛣️ Travel Details</h3>
+      <div>{details ? <div dangerouslySetInnerHTML={{__html:details}} /> : null}</div>
+      <button onClick={onClose}>Close</button>
+    </Modal>
+  );
+}
 
