39450603c5aade3218d9cf3b7b5ac6a4282a5f99
manikwork50 <manikwork50@gmail.com>
2025-08-16 20:28:44 +0530

    Update MapView.js

--- frontend/src/components/MapView.js
+++ frontend/src/components/MapView.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import { Map, GoogleApiWrapper, DirectionsRenderer } from 'google-maps-react';
+
+function MapView({ google, directions }) {
+  return (
+    <div style={{ width: "100%", height: "60vh" }}>
+      <Map
+        google={google}
+        zoom={5}
+        initialCenter={{ lat: 22.9734, lng: 78.6569 }}
+        style={{ width:'100%', height:'100%' }}
+        disableDefaultUI={true}
+      >
+        {directions && <DirectionsRenderer directions={directions}/>}
+      </Map>
+    </div>
+  );
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'YOUR_GOOGLE_MAPS_API_KEY',
+  libraries: ['places']
+})(MapView);
 
